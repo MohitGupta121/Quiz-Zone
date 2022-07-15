7b67c834d09ad731300e4b8603b3a1b940214abb
MohitGupta121 <guptamm.22@gmail.com>
2022-07-15 15:36:37 +0530

    implemented firebase, recyclerview, result activity

---
+++ app/src/main/java/com/mohit/quizzone/activities/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.mohit.quizzone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.mohit.quizzone.R
+import com.mohit.quizzone.adapters.QuizAdapter
+import com.mohit.quizzone.models.Quiz
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: QuizAdapter
+    private var quizList = mutableListOf<Quiz>()
+    lateinit var firestore: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpViews()
+    }
+
+    fun setUpViews() {
+        setUpFireStore()
+        setUpRecyclerView()
+    }
+
+    private fun setUpFireStore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference = firestore.collection("quizzes")
+        collectionReference.addSnapshotListener { value, error ->
+            if(value == null || error != null){
+                Toast.makeText(this, "Error fetching data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+            quizList.clear()
+            quizList.addAll(value.toObjects(Quiz::class.java))
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        adapter = QuizAdapter(this, quizList)
+        quizRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        quizRecyclerView.adapter = adapter
+    }
+
+}
---
+++ app/src/main/java/com/mohit/quizzone/adapters/QuizAdapter.kt
@@ -0,0 +1,48 @@
+package com.mohit.quizzone.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.mohit.quizzone.R
+import com.mohit.quizzone.activities.QuestionActivity
+import com.mohit.quizzone.models.Quiz
+import com.mohit.quizzone.utils.ColorPicker
+import com.mohit.quizzone.utils.IconPicker
+
+class QuizAdapter(val context: Context, val quizzes: List<Quiz>) :
+    RecyclerView.Adapter<QuizAdapter.QuizViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuizViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.quiz_item, parent, false)
+        return QuizViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
+        holder.textViewTitle.text = quizzes[position].title
+        holder.cardContainer.setCardBackgroundColor(Color.parseColor(ColorPicker.getColor()))
+        holder.iconView.setImageResource(IconPicker.getIcon())
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, quizzes[position].title, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, QuestionActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return quizzes.size
+    }
+
+    inner class QuizViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewTitle: TextView = itemView.findViewById(R.id.quizTitle)
+        var iconView: ImageView = itemView.findViewById(R.id.quizIcon)
+        var cardContainer: CardView = itemView.findViewById(R.id.cardContainer)
+    }
+}
